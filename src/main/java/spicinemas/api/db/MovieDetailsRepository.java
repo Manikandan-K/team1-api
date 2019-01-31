2dfdabb533503dfc85a16027a291f0f0b3dbe629
avinash.saraf <avinash.saraf@thoughtworks.com>
2019-01-31 10:42:26 +0530

    sadhna/avinash: story-5 creating model and test classes

---
+++ src/main/java/spicinemas/api/db/MovieDetailsRepository.java
@@ -0,0 +1,14 @@
+package spicinemas.api.db;
+
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class MovieDetailsRepository {
+
+	@Autowired
+	private DSLContext dsl;
+}
